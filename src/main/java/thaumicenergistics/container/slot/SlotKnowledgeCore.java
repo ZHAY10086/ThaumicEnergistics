7cf4c604b631d57b18752054f78b85b0c9ca648a
Delfayne <delfayne@users.noreply.github.com>
2026-07-15 20:49:12 +0900

    Allow swapping knowledge cores from slots if you have one on the cursor

--- src/main/java/thaumicenergistics/container/slot/SlotKnowledgeCore.java
+++ src/main/java/thaumicenergistics/container/slot/SlotKnowledgeCore.java
@@ -1,8 +1,10 @@
 package thaumicenergistics.container.slot;
 
+import net.minecraft.item.ItemStack;
 import net.minecraftforge.items.IItemHandler;
 
 import thaumicenergistics.ThaumicEnergisticsApi;
+import thaumicenergistics.item.ItemKnowledgeCore;
 
 import javax.annotation.Nullable;
 
@@ -12,6 +14,19 @@ import javax.annotation.Nullable;
 public class SlotKnowledgeCore extends ThESlot {
     public SlotKnowledgeCore(IItemHandler handler, int index, int xPosition, int yPosition) {
         super(handler, index, xPosition, yPosition);
+    }
+
+    /**
+     * This is the dedicated core slot, so always accept a knowledge core. The backing upgrade
+     * inventory rejects a core once one is installed, which is correct for insertion but blocks
+     * vanilla's swap-click - that gate also relies on isItemValid. Since there is only ever one
+     * physical slot, accepting here can't over-fill; it just lets a core on the cursor swap with
+     * the one already present.
+     */
+    @Override
+    public boolean isItemValid(ItemStack stack) {
+        if (stack.getItem() instanceof ItemKnowledgeCore) return true;
+        return super.isItemValid(stack);
     }
 
     @Nullable
