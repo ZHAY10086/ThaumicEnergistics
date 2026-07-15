95947171968d8168c03d8b59853c977f257e1a51
Delfayne <delfayne@users.noreply.github.com>
2026-07-15 20:40:05 +0900

    Shows what recipes are in which knowledge core on tooltip

--- src/main/java/thaumicenergistics/item/ItemKnowledgeCore.java
+++ src/main/java/thaumicenergistics/item/ItemKnowledgeCore.java
@@ -1,6 +1,20 @@
 package thaumicenergistics.item;
 
+import net.minecraft.client.gui.GuiScreen;
+import net.minecraft.client.resources.I18n;
+import net.minecraft.client.util.ITooltipFlag;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.text.TextFormatting;
+import net.minecraft.world.World;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+
 import thaumicenergistics.util.KnowledgeCoreUtil;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import javax.annotation.Nullable;
 
 /**
  * If you're looking for methods to operate on a Knowledge Core ItemStack and its recipes, check out
@@ -19,5 +33,32 @@ public class ItemKnowledgeCore extends ItemMaterial {
 
     public boolean isBlank() {
         return this.isBlank;
+    }
+
+    @Override
+    @SideOnly(Side.CLIENT)
+    public void addInformation(
+            ItemStack stack, @Nullable World world, List<String> tooltip, ITooltipFlag flag) {
+        super.addInformation(stack, world, tooltip, flag);
+        if (this.isBlank) return;
+
+        List<KnowledgeCoreUtil.Recipe> recipes =
+                KnowledgeCoreUtil.recipeStreamOf(stack).collect(Collectors.toList());
+        if (recipes.isEmpty()) return;
+
+        tooltip.add(
+                TextFormatting.GRAY
+                        + I18n.format(
+                                "tooltip.thaumicenergistics.knowledge_core.recipes",
+                                recipes.size()));
+        if (GuiScreen.isShiftKeyDown()) {
+            for (KnowledgeCoreUtil.Recipe recipe : recipes) {
+                tooltip.add(TextFormatting.DARK_GRAY + " - " + recipe.getResult().getDisplayName());
+            }
+        } else {
+            tooltip.add(
+                    TextFormatting.DARK_GRAY
+                            + I18n.format("tooltip.thaumicenergistics.knowledge_core.hold_shift"));
+        }
     }
 }
